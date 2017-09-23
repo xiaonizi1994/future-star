7041a7939e539bd95d32c758f97bfa50abb54c31
twcn <twcn@twcns-MacBook-Pro-3.local>
2017-09-23 22:14:22 +0800

    create login api

---
+++ src/main/java/com/future/star/api/LoginController.java
@@ -0,0 +1,27 @@
+package com.future.star.api;
+
+import com.future.star.dto.UserDTO;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("api/login")
+public class LoginController {
+    @PostMapping
+    public Map<String,String> login(@RequestBody UserDTO userDTO){
+        Map<String,String> map=new HashMap<>();
+        if(userDTO.getName().equals("future")&&userDTO.getPassword().equals("123")){
+            map.put("message","login sucessful");
+            return map;
+        }
+        else{
+            map.put("message","login failed");
+        }
+        return map;
+    }
+}
--- src/main/java/com/future/star/dto/UserDTO.java
+++ src/main/java/com/future/star/dto/UserDTO.java
@@ -4,4 +4,19 @@ package com.future.star.dto;
  * Created by twcn on 23/09/2017.
  */
 public class UserDTO {
+    String name;
+    String password;
+    int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public int getAge() {
+        return age;
+    }
 }
